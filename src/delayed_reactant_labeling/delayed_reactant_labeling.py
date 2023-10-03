5b853faccdf833612e52fab09f3404be6dac69b6
Martijn Dingemans <Martijn.Dingemans@ru.nl>
2023-10-03 16:07:34 +0200

    Added a way to alter the number of steps simulated in between each recorded step.

--- src/delayed_reactant_labeling/delayed_reactant_labeling.py
+++ src/delayed_reactant_labeling/delayed_reactant_labeling.py
@@ -92,62 +92,54 @@ class DRL:
             self.reaction_products.append(np.array([self.reference[product] for product in products]))
         self.reaction_rate = np.array(self.reaction_rate)
 
-    def _predict_concentration_slice(self, initial_concentration: np.ndarray, time_slice: np.ndarray, mass_balance: list[str]) -> pd.DataFrame:
+    def _predict_concentration_slice(self,
+                                     initial_concentration: np.ndarray,
+                                     time_slice: np.ndarray,
+                                     steps_per_step: int) -> tuple[pd.DataFrame, np.ndarray]:
         """
         Predicts the concentration of a singular time slice.
         :param initial_concentration: The initial concentration of the system.
         :param time_slice: The datapoints that must be recorded.
-        :return prediction: pd.Dataframe of the prediction.
+        :param steps_per_step: The number of steps to simulate inbetween each step in the time slice.
+        Higher values yield higher accuracy at the cost of computation time.
+        :return prediction: pd.Dataframe of the prediction and a np.ndarray of the last prediction step.
         """
-        prev_prediction = initial_concentration
         predicted_concentration = np.full((len(time_slice), len(initial_concentration)), np.nan)
         predicted_concentration[0, :] = initial_concentration
-
-        # for the first step more steps are required; as by definition, no 5 or 6 could be formed in a singular step.
         prev_t = time_slice[0]
-        new_prediction = None
-        for new_t in np.linspace(prev_t, time_slice[1], 30)[1:]:
-            new_prediction = calculate_step(
-                reaction_rate=self.reaction_rate,
-                reaction_reactants=self.reaction_reactants,
-                reaction_products=self.reaction_products,
-                concentrations=prev_prediction,
-                delta_time=new_t - prev_t)
-
-            prev_t = new_t
-            prev_prediction = new_prediction
-        predicted_concentration[1, :] = new_prediction
 
         # use the given steps
-        for row, new_t in enumerate(time_slice[2:]):
-            new_prediction = calculate_step(
-                reaction_rate=self.reaction_rate,
-                reaction_reactants=self.reaction_reactants,
-                reaction_products=self.reaction_products,
-                concentrations=prev_prediction,
-                delta_time=new_t - prev_t, )
+        prediction_step = initial_concentration
+        for row, next_spectra_t in enumerate(time_slice[1:]):
+            in_between_steps = np.linspace(prev_t, next_spectra_t, steps_per_step + 1)[1:]  # ignore start
+            for new_t in in_between_steps:
+                prediction_step = calculate_step(
+                    reaction_rate=self.reaction_rate,
+                    reaction_reactants=self.reaction_reactants,
+                    reaction_products=self.reaction_products,
+                    concentrations=prediction_step,
+                    delta_time=new_t - prev_t, )
+                prev_t = new_t
 
-            predicted_concentration[row + 2, :] = new_prediction
-            prev_t = new_t
-            prev_prediction = new_prediction
+            if any(prediction_step < 0):
+                raise ValueError("Negative concentrations were found, increase the steps per step to resolve this. "
+                                 f"\n\tThis happened during iteration {row}")
+            predicted_concentration[row + 1, :] = prediction_step
+
         df_result = pd.DataFrame(predicted_concentration, columns=list(self.reference.keys()))
-        df_result["time (min)"] = time_slice
-        last_prediction = prev_prediction
+        df_result["time"] = time_slice
 
-        if mass_balance is not None:
-            mass_sum = np.sum(predicted_concentration[:, [self.reference[chemical] for chemical in mass_balance]],
-                              axis=1)
-            if not all(mass_sum - mass_sum[0] < 1e-14):
-                raise ValueError("The mass balance was not obeyed.")
-
-        return df_result, last_prediction
+        return df_result, prediction_step  # last prediction step
 
     def predict_concentration(self,
-                              experimental_conditions: Experimental_Conditions
+                              experimental_conditions: Experimental_Conditions,
+                              steps_per_step: int = 1,
                               ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         Predicts the concentration of a system, using the appropriate experimental conditions.
         :param experimental_conditions: Experimental Conditions object.
+        :param steps_per_step: The number of steps to simulate inbetween each step in the time slice.
+        Higher values yield higher accuracy at the cost of computation time.
         :return (unlabeled prediction, labeled prediction,): Pd.Dataframe of the situation pre-addition of the labeled
          compound, and one of the post-addition situation.
         """
@@ -159,7 +151,7 @@ class DRL:
         result_pre_addition, last_prediction = self._predict_concentration_slice(
             initial_concentration=self.initial_concentrations,
             time_slice=experimental_conditions.time[0],
-            mass_balance=experimental_conditions.mass_balance
+            steps_per_step=steps_per_step
         )
 
         # dillution step
@@ -171,7 +163,7 @@ class DRL:
         results_post_addition, _ = self._predict_concentration_slice(
             initial_concentration=diluted_concentrations,
             time_slice=experimental_conditions.time[1],
-            mass_balance=experimental_conditions.mass_balance
+            steps_per_step=steps_per_step
         )
         return result_pre_addition, results_post_addition
 
